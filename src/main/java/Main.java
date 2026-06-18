bea7bf54c54e65a3502702b90db25be06f884283
Shreya-S-Jadhav <s.s.shreya.jadhav@gmail.com>
2026-06-18 17:16:16 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in); 
         // TODO: Uncomment the code below to pass the first stage
+        while (true){
         System.out.print("$ ");
         String s = sc.nextLine();
         System.out.println(s + ": command not found");
+        }
     }
 }
